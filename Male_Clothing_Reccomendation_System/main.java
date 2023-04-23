9c8c369c238edd897a557c488f6fac0be3c2ad7c
KingzShady <eomotoshom@gmail.com>
2023-04-23 12:49:13 +0100

    Made image into an array
    made Jframe into a gallery, and seperated the images

--- Male_Clothing_Reccomendation_System/main.java
+++ Male_Clothing_Reccomendation_System/main.java
@@ -1,5 +1,5 @@
 package Male_Clothing_Reccomendation_System;
-
+import java.awt.GridLayout;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -7,23 +7,34 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public class main {
     public static void main(String[] args) {
-        String imagePath = "C:\\Users\\eomot\\OneDrive\\Desktop\\Job Search & Career Records\\Portfolio Website\\Java Projects\\Male_Clothing_Reccomendation_System\\Datasets\\benchmark images\\male body types.jpg";
+        String[] imagePaths = 
+        {"C:\\Users\\eomot\\OneDrive\\Desktop\\Job Search & Career Records\\Portfolio Website\\Java Projects\\Male_Clothing_Reccomendation_System\\Datasets\\benchmark images\\Ectomorph.jpg",
+         "C:\\Users\\eomot\\OneDrive\\Desktop\\Job Search & Career Records\\Portfolio Website\\Java Projects\\Male_Clothing_Reccomendation_System\\Datasets\\benchmark images\\Endomorph.jpg", 
+         "C:\\Users\\eomot\\OneDrive\\Desktop\\Job Search & Career Records\\Portfolio Website\\Java Projects\\Male_Clothing_Reccomendation_System\\Datasets\\benchmark images\\Mesomorph.jpg"};
 
-        try {
-            BufferedImage image = ImageIO.read(new File(imagePath));
-            ImageIcon icon = new ImageIcon(image);
-            JLabel label = new JLabel(icon);
-            JFrame frame = new JFrame();
-            frame.getContentPane().add(label);
-            frame.pack();
-            frame.setVisible(true);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        } catch (IOException e) {
-            System.out.println("Error loading image: " + e.getMessage());
+        JPanel panel = new JPanel(new GridLayout(0, 3));
+        JScrollPane scrollPane = new JScrollPane(panel);
+        JFrame frame = new JFrame();
+        frame.add(scrollPane);
+        
+        for (String imagePath : imagePaths) {
+            try {
+                BufferedImage image = ImageIO.read(new File(imagePath));
+                ImageIcon icon = new ImageIcon(image);
+                JLabel label = new JLabel(icon);
+                panel.add(label);
+            } catch (IOException e) {
+                System.out.println("Error loading image: " + e.getMessage());
+            }
         }
+
+        frame.pack();
+        frame.setVisible(true);
     }
 }
         
