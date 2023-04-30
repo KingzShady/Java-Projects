f8afa123eaa3e4b5f1f49d283062fd2bcdefcfbc
KingzShady <eomotoshom@gmail.com>
2023-04-30 14:00:55 +0100

    Creating Looking for Routes Method
    - Added the Function
    - added to the function if the current node to the route and the finished route to the vector of all routes if there are no more nodes to add.
    - Looping over all remaining nodes
    - Removing the current node to create a new set of remaining nodes
    - Creating a fresh route by adding the current node
    - Finding the separation between the current node and the following node
    - Calling findRoutes recursively with the updated current node, updated nodes that are left, and updated route
    - Continually finding all routes
    -  Printing all the routes

--- Travelling Sales man/src/TravelingSalesmanProblem.java
+++ Travelling Sales man/src/TravelingSalesmanProblem.java
@@ -26,14 +26,48 @@ public class TravelingSalesmanProblem {
             }
         }
         
-        // Finding all the routes
+        /* Finding all the routes */
+        // Creating a vector to store all routes found
         Vector<Vector<Integer>> routes = new Vector<Vector<Integer>>();
+
+        // Creating a vector to store all the nodes that have been visited
         Vector <Integer> currentRoute = new Vector<Integer>();
         currentRoute.add(startNode);
-        routes.add(currentRoute);
-        return;
 
-        
+        // Continually finding all routes
+        findRoutes(startNode, Nodes, distances, currentRoute, routes);
 
+        // Printing all the routes
+        System.out.println("All the routes are: " + routes);
+    }
+
+    /* Method used to find all the routes between the nodes */
+    public static void findRoutes(int currentNode, Vector<Integer> nodes,
+    int[][] distances, Vector<Integer> currentRoute, Vector<Vector<Integer>> routes) {
+        // Add the current node to the route and the finished route to the vector of all routes if there are no more nodes to add.
+        if (nodes.size() == 0) {
+            currentRoute.add(currentNode);
+            routes.add(currentRoute);
+            return;
+        }
+
+        // Looping over all remaining nodes
+        for (int i = 0 ; i < nodes.size(); i++) {
+            int nextNode = nodes.get(i);
+
+            // Removing the current node to create a new set of remaining nodes
+            Vector<Integer> newNode = new Vector<Integer>(nodes);
+            newNode.removeElementAt(i);
+
+            // Creating a fresh route by adding the current node
+            Vector<Integer> newRoute = new Vector<Integer>(currentRoute);
+            newRoute.add(nextNode);
+
+            // Finding the separation between the current node and the following node
+            int newDistance = distances[currentNode][nextNode];
+
+            // Calling findRoutes recursively with the updated current node, updated nodes that are left, and updated route
+            findRoutes(nextNode, newNode, distances, newRoute, routes);
+        }
     }
 }
